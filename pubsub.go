21a6bddae91870322e9d41a4631a45b936880e1c
mrcampa404 <61489449+mrcampa404@users.noreply.github.com>
2021-01-05 04:08:22 -0600

    Add files via upload

---
+++ pubsub.go
@@ -0,0 +1,39 @@
+package pubsub
+
+import (
+	"encoding/json"
+	"gopkg.in/redis.v2"
+)
+
+type PubSub struct {
+	client *redis.Client
+}
+
+var Service *PubSub
+
+func init() {
+	var client *redis.Client
+	client := redis.NewClient(&redis.Options{
+		//la db de google 
+		Addr:     "redis-18582.c12281.us-central1-mz.gcp.cloud.rlrcp.com:18582",
+		Password: "WKJeO6VNC7tZX6ae672rbXNJUUXCy3Wm",
+		//Addr:     "localhost:6379",
+		//Password: "",
+		DB:       0,
+	})
+	Service = &PubSub{client}
+}
+
+func (ps *PubSub) PublishString(channel, message string) *redis.IntCmd {
+	return ps.client.Publish(channel, message)
+}
+
+func (ps *PubSub) Publish(channel string, message interface{}) *redis.IntCmd {
+	// TODO reflect if interface{} type is string, Publish as-is
+	jsonBytes, err := json.Marshal(message)
+	if err != nil {
+		panic(err)
+	}
+	messageString := string(jsonBytes)
+	return ps.client.Publish(channel, messageString)
+}
